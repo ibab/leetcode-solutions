f85c673626dfdbde61756381b4e31cc9fbba61b6
Igor Babuschkin <igor@babuschk.in>
2016-10-01 15:25:42 +0200

    Update solution to 69

--- 069/main.cc
+++ 069/main.cc
@@ -3,6 +3,25 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        unsigned long left = 0;
+        unsigned long right = INT_MAX;
+        while (left + 1 < right) {
+            unsigned long mid = left + (right - left) / 2;
+            if (mid * mid <= x) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
+
+// Alternative solution using Newton method.
+/*
+class Solution {
+public:
+    int mySqrt(int x) {
         double y = 1;
         for (int i = 0; i < 20; i++) {
             y -= (y * y - x) / (2 * y);
@@ -10,6 +29,7 @@ public:
         return y;
     }
 };
+*/
 
 int main() {
     std::cout << Solution().mySqrt(2147395599) << std::endl;
