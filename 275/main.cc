13186eefcaded158e14a1cb18941e2c6d51b293b
Igor Babuschkin <igor@babuschk.in>
2016-10-16 01:09:51 +0200

    Add solution to 275

---
+++ 275/main.cc
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        if (citations.empty()) {
+            return 0;
+        }
+        int n = citations.size();
+        int h = 0;
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int h_ = n - mid;
+            if (citations[mid] >= h_) {
+                // Move left.
+                right = mid;
+            } else {
+                // Move right.
+                left = mid + 1;
+            }
+        }
+        return n - left;
+    }
+};
